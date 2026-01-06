30655e5dffc12deb67144127690d913f215f7a8b
amanbhaatia <AMANDB1414@GMAIL.COM>
2026-01-07 01:37:09 +0530

    Add SVG social icons with handles in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -15,22 +15,44 @@ export default function Footer() {
           </div>
 
           {/* Social Links */}
-          <div className="flex gap-8 text-sm text-gray-400">
-            <a
-              href="https://www.instagram.com/amanblifts"
-              target="_blank"
-              className="hover:text-white transition"
-            >
-              Instagram
-            </a>
-            <a
-              href="https://www.youtube.com/@amanblifts"
-              target="_blank"
-              className="hover:text-white transition"
-            >
-              YouTube
-            </a>
-          </div>
+          <div className="flex flex-col sm:flex-row gap-6 text-sm text-gray-400">
+  
+  {/* YouTube */}
+  <a
+    href="https://www.youtube.com/@amanblifts"
+    target="_blank"
+    className="flex items-center gap-2 hover:text-white transition"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-5 h-5"
+    >
+      <path d="M23.498 6.186a2.999 2.999 0 00-2.114-2.12C19.505 3.5 12 3.5 12 3.5s-7.505 0-9.384.566a2.999 2.999 0 00-2.114 2.12A31.6 31.6 0 000 12a31.6 31.6 0 00.502 5.814 2.999 2.999 0 002.114 2.12C4.495 20.5 12 20.5 12 20.5s7.505 0 9.384-.566a2.999 2.999 0 002.114-2.12A31.6 31.6 0 0024 12a31.6 31.6 0 00-.502-5.814zM9.75 15.568V8.432L15.818 12 9.75 15.568z" />
+    </svg>
+    <span>@amanblifts</span>
+  </a>
+
+  {/* Instagram */}
+  <a
+    href="https://www.instagram.com/amanblifts"
+    target="_blank"
+    className="flex items-center gap-2 hover:text-white transition"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-5 h-5"
+    >
+      <path d="M7.75 2h8.5A5.75 5.75 0 0122 7.75v8.5A5.75 5.75 0 0116.25 22h-8.5A5.75 5.75 0 012 16.25v-8.5A5.75 5.75 0 017.75 2zm0 1.5A4.25 4.25 0 003.5 7.75v8.5A4.25 4.25 0 007.75 20.5h8.5a4.25 4.25 0 004.25-4.25v-8.5A4.25 4.25 0 0016.25 3.5h-8.5zM12 7a5 5 0 100 10 5 5 0 000-10zm0 1.5a3.5 3.5 0 110 7 3.5 3.5 0 010-7zm5.25-.88a.88.88 0 110 1.75.88.88 0 010-1.75z" />
+    </svg>
+    <span>@amanblifts</span>
+  </a>
+
+</div>
+
 
         </div>
 
