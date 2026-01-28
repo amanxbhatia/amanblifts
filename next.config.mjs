a8c182df90ac5ff37e97d4ad76d0e9a34d7613e9
amanbhaatia <AMANDB1414@GMAIL.COM>
2026-01-29 00:59:36 +0530

    Allow YouTube thumbnails via remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   /* config options here */
-// };
-
-// export default nextConfig;
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["img.youtube.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.youtube.com",
+      },
+    ],
   },
 };
 
