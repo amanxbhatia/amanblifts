0c77162781dcbc48ac62dc2357414dccebd35227
amanbhaatia <AMANDB1414@GMAIL.COM>
2026-01-31 00:28:01 +0530

    Fix production thumbnails using unoptimized images

--- src/app/exercises/page.js
+++ src/app/exercises/page.js
@@ -67,6 +67,7 @@ export default function ExercisesPage() {
                       fill
                       className="object-cover"
                       sizes="(max-width: 768px) 100vw, 50vw"
+                      unoptimized
                     />
                   </div>
                 </div>
